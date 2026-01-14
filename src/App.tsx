609a5787fc5fef32aeeedee9f0420b96526aa84c
chaiyapat boonmasungsong <makchaiyapat@gmail.com>
2026-01-15 01:46:04 +0700

    state 03 finish

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { getInvestigatorHint } from './services/genimiService';
 
 const App: React.FC = () => {
   const [status, setStatus] = useState<CaseStatus>({
-    stage: GameStage.AUTHZ,
+    stage: GameStage.SPLASH,
     timer: 0,
     hintsUsed: 0,
     progress: 0,
